1671b68cd542ff8b2a3be90a17d205cfde3f5a72
Mohamed Elkhaloui <elkhaloui@gmail.com>
2020-10-31 14:23:07 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -25,30 +25,5 @@ row #=> ["0", "X", "2"]
 # Move X to the Position 3, Right
 row["2"] = "X"
 row #=> ["0", "1", "x"]
-row = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-# position 1: Left 
-# position 1: Left 
-row[0]
+row = [ "0", "1", "3"]
 
-# position 2: Middle 
-row[1]
-
-# position 3: Right 
-row[2]
-
-# Move X to the Position 2, Middle
-row[1] = "X"
-row #=> ["0", "X", "2", "3",
-
-# position 1: Left 
-row[0]
-
-# position 2: Middle 
-row[1]
-
-# position 3: Right 
-row[2]
-
-# Move X to the Position 2, Middle
-row[1] = "X"
-row #=> ["0", "X", "2", "3",
