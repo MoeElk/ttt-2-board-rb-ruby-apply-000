2fbbbbe96a7aeb3e2604622d8ac32bbc853052c7
Mohamed Elkhaloui <elkhaloui@gmail.com>
2020-10-31 14:17:08 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -4,8 +4,6 @@
 ---------
 7 | 8|  9
 
-
-"1" = 
 row = [ "0", "1", "2"]
 # position 1: Left 
 row[0]
@@ -18,7 +16,7 @@ row[2]
 
 # Move X to the Position 2, Middle
 row[1] = "X"
-row #=> ["0", "X", "2", "3", "4", "5", "6", "7", "8", "9"]
+row #=> ["0", "X", "2"]
 
 row = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 # position 1: Left 
@@ -35,3 +33,15 @@ row[2]
 row[1] = "X"
 row #=> ["0", "X", "2", "3",
 
+# position 1: Left 
+row[0]
+
+# position 2: Middle 
+row[1]
+
+# position 3: Right 
+row[2]
+
+# Move X to the Position 2, Middle
+row[1] = "X"
+row #=> ["0", "X", "2", "3",
