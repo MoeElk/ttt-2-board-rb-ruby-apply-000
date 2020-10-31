7903c8257f8576d867698a329f03874ec446c076
Mohamed Elkhaloui <elkhaloui@gmail.com>
2020-10-31 14:46:43 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,66 +1,66 @@
-row = [ "1", "2", "3"]
+row = ["1", "2", "3"]
 # Position 1: Left 
 row[0]
 
 # Position 2: Middle 
 row[1]
 
-# position 3: Right 
+# Position 3: Right 
 row[2]
 
 # Move X to the Position 1, Left
-row["0"] = "X"
+row[0] = "X"
 row #=> ["X", "1", "2"]
 
 # Move X to the Position 2, Middle
-row["1"] = "X"
+row[1] = "X"
 row #=> ["0", "X", "2"]
 
 # Move X to the Position 3, Right
-row["2"] = "X"
+row[2] = "X"
 row #=> ["0", "1", "x"]
 
-row = [ "4", "5", "6"]
-# position 4: Left 
+row = ["4", "5", "6"]
+# Position 4: Left 
 row[3]
 
-# position 5: Middle 
+# Position 5: Middle 
 row[4]
 
-# position 6: Right 
+# Position 6: Right 
 row[5]
 
 # Move X to the Position 4, Left
-row["4"] = "X"
+row[3] = "X"
 row #=> ["X", "5", "6"]
 
 # Move X to the Position 5, Middle
-row["5"] = "X"
+row[4] = "X"
 row #=> ["4", "X", "6"]
 
 # Move X to the Position 6, Right
-row["6"] = "X"
+row[5] = "X"
 row #=> ["4", "5", "x"]
 
-row = [ "7", "1", "2"]
-# position 1: Left 
-row[0]
+row = ["7", "8", "9"]
+# Position 1: Left 
+row[6]
 
-# position 2: Middle 
-row[1]
+# Position 2: Middle 
+row[7]
 
-# position 3: Right 
-row[2]
+# Position 3: Right 
+row[8]
 
 # Move X to the Position 1, Left
-row["0"] = "X"
-row #=> ["X", "1", "2"]
+row[6] = "X"
+row #=> ["X", "7", "8"]
 
 # Move X to the Position 2, Middle
-row["1"] = "X"
-row #=> ["0", "X", "2"]
+row[7] = "X"
+row #=> ["6", "X", "8"]
 
 # Move X to the Position 3, Right
-row["2"] = "X"
-row #=> ["0", "1", "x"]
+row[8] = "X"
+row #=> ["6", "7", "x"]
 row = [ "0", "1", "3"]
