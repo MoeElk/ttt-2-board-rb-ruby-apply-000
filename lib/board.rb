62232b1fd2e389abfc1c6b23392388b0a32f45cf
Mohamed Elkhaloui <elkhaloui@gmail.com>
2020-10-31 15:03:06 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -12,15 +12,15 @@ row[2]
 
 # Move X to the Position 1, Left
 row[0] = "X"
-row #=> ["X", "1", "2"]
+row #=> ["X", " ", " "]
 
 # Move X to the Position 2, Middle
 row[1] = "X"
-row #=> ["0", "X", "2"]
+row #=> [" ", "X", " "]
 
 # Move X to the Position 3, Right
 row[2] = "X"
-row #=> ["0", "1", "x"]
+row #=> [" ", " ", "x"]
 
 row = ["4", "5", "6"]
 # Position 4: Left 
@@ -34,15 +34,15 @@ row[5]
 
 # Move X to the Position 4, Left
 row[3] = "X"
-row #=> ["X", "5", "6"]
+row #=> ["X", " ", " "]
 
 # Move X to the Position 5, Middle
 row[4] = "X"
-row #=> ["4", "X", "6"]
+row #=> [" ", "X", " "]
 
 # Move X to the Position 6, Right
 row[5] = "X"
-row #=> ["4", "5", "x"]
+row #=> [" ", " ", "x"]
 
 row = ["7", "8", "9"]
 # Position 1: Left 
@@ -56,12 +56,12 @@ row[8]
 
 # Move X to the Position 1, Left
 row[6] = "X"
-row #=> ["X", "7", "8"]
+row #=> ["X", " ", " "]
 
 # Move X to the Position 2, Middle
 row[7] = "X"
-row #=> ["6", "X", "8"]
+row #=> [" ", "X", " "]
 
 # Move X to the Position 3, Right
 row[8] = "X"
-row #=> ["6", "7", "x"]
+row #=> [" ", " ", "x"]
