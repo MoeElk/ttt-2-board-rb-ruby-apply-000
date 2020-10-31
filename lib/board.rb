e0b03fa743947eeb8af5efd911ec72a5d0d017e1
Mohamed Elkhaloui <elkhaloui@gmail.com>
2020-10-31 14:20:08 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -14,9 +14,12 @@ row[1]
 # position 3: Right 
 row[2]
 
+# Move X to the Position 1, Left
+row["0"] = "X"
+row #=> ["X", "1", "2"]
 
 # Move X to the Position 2, Middle
-row[1] = "X"
+row["1"] = "X"
 row #=> ["0", "X", "2"]
 
 row = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
